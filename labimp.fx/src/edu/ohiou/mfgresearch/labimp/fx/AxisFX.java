d3ba5905a5144b363ab716b1e486cf913cc1aaca
Arif Ahmed <arifahmed411@gmail.com>
2016-08-31 17:54:33 -0400

    Updated AxisFX class.

--- labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/AxisFX.java
+++ labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/AxisFX.java
@@ -8,6 +8,7 @@ import javafx.scene.shape.Box;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 import javafx.scene.shape.Shape3D;
+import javafx.scene.transform.Rotate;
 
 public class AxisFX extends FXObject {
 	
@@ -29,36 +30,57 @@ public class AxisFX extends FXObject {
 		this.zLength = zLength;
 	}
 	
-	public LinkedList<Shape3D> getFX3DShapes() {
+	public LinkedList<Shape> getFXShapes() {
+		LinkedList<Shape> axes = new LinkedList<>();
 		
-		LinkedList<Shape3D> shapes = new LinkedList<>();
+		Line xAxis = new Line(0, 0, xLength, 0);
+		Line yAxis = new Line(0, 0, 0, yLength);
+		Line zAxis = new Line(0, 0, xLength, 0);
 		
-        Box xAxis = new Box(xLength, 1, 1);
-        Box yAxis = new Box(1, yLength, 1);
-        Box zAxis = new Box(1, 1, zLength);
+		zAxis.setRotationAxis(Rotate.Y_AXIS);
+		zAxis.setRotate(90);
 		
-		shapes.add(xAxis);
-		shapes.add(yAxis);
-		shapes.add(zAxis);
+		xAxis.setStroke(Color.RED);
+		yAxis.setStroke(Color.GREEN);
+		zAxis.setStroke(Color.BLUE);
 		
-        final PhongMaterial redMaterial = new PhongMaterial();
-        redMaterial.setDiffuseColor(Color.DARKRED);
-        redMaterial.setSpecularColor(Color.RED);
-
-        final PhongMaterial greenMaterial = new PhongMaterial();
-        greenMaterial.setDiffuseColor(Color.DARKGREEN);
-        greenMaterial.setSpecularColor(Color.GREEN);
-
-        final PhongMaterial blueMaterial = new PhongMaterial();
-        blueMaterial.setDiffuseColor(Color.DARKBLUE);
-        blueMaterial.setSpecularColor(Color.BLUE);
-        
-        xAxis.setMaterial(redMaterial);
-        yAxis.setMaterial(greenMaterial);
-        zAxis.setMaterial(blueMaterial);
-
-		return shapes;
+		axes.add(xAxis);
+		axes.add(yAxis);
+		axes.add(zAxis);
+		
+		return axes;
 	}
+	
+//	public LinkedList<Shape3D> getFX3DShapes() {
+//		
+//		LinkedList<Shape3D> shapes = new LinkedList<>();
+//		
+//        Box xAxis = new Box(xLength, 1, 1);
+//        Box yAxis = new Box(1, yLength, 1);
+//        Box zAxis = new Box(1, 1, zLength);
+//		
+//		shapes.add(xAxis);
+//		shapes.add(yAxis);
+//		shapes.add(zAxis);
+//		
+//        final PhongMaterial redMaterial = new PhongMaterial();
+//        redMaterial.setDiffuseColor(Color.DARKRED);
+//        redMaterial.setSpecularColor(Color.RED);
+//
+//        final PhongMaterial greenMaterial = new PhongMaterial();
+//        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+//        greenMaterial.setSpecularColor(Color.GREEN);
+//
+//        final PhongMaterial blueMaterial = new PhongMaterial();
+//        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+//        blueMaterial.setSpecularColor(Color.BLUE);
+//        
+//        xAxis.setMaterial(redMaterial);
+//        yAxis.setMaterial(greenMaterial);
+//        zAxis.setMaterial(blueMaterial);
+//
+//		return shapes;
+//	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
