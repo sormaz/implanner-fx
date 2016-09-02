b75cc16872e21b4c4bf7c497d9dc0de2ae85e824
Arif Ahmed <arifahmed411@gmail.com>
2016-09-02 18:54:10 -0400

    getting Part Name from part model.

--- labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/PartModelConverter.java
+++ labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/PartModelConverter.java
@@ -3,12 +3,15 @@ package edu.ohiou.mfgresearch.labimp.fx;
 import java.io.File;
 
 import edu.ohiou.mfgresearch.implanner.geometry.PartModel;
+import edu.ohiou.mfgresearch.implanner.parts.MfgPartModel;
 import edu.ohiou.mfgresearch.labimp.draw.ImpObject;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class PartModelConverter extends Swing3DConverter {
 	
 	public PartModelConverter(File file) {
-		this(new PartModel(file));
+		this(new MfgPartModel(new PartModel(file)));
 	}
 
 	public PartModelConverter(ImpObject swingTarget) {
@@ -17,6 +20,11 @@ public class PartModelConverter extends Swing3DConverter {
 
 	public PartModelConverter(DrawFXCanvas parentContainer, ImpObject swingTarget) {
 		super(parentContainer, swingTarget);
+	}
+	
+	public StringProperty name() {
+		return new SimpleStringProperty
+				(((MfgPartModel)getSwingTarget()).getPartName());
 	}
 
 }
