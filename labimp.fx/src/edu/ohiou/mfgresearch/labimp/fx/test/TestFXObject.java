3792175ed08e8449783b74afd7880e6481d595b3
cd-tsa-ohio <sormaz@ohio.edu>
2016-09-24 20:00:30 -0400

    implemented actual test that shows 3d shapes

--- labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/test/TestFXObject.java
+++ labimp.fx/src/edu/ohiou/mfgresearch/labimp/fx/test/TestFXObject.java
@@ -1,5 +1,12 @@
 package edu.ohiou.mfgresearch.labimp.fx.test;
 
+import java.util.LinkedList;
+
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.shape.Sphere;
+import javafx.scene.text.Text;
 import edu.ohiou.mfgresearch.labimp.fx.DrawFXCanvas;
 import edu.ohiou.mfgresearch.labimp.fx.FXObject;
 
@@ -14,6 +21,23 @@ public class TestFXObject extends FXObject {
 //		super(parentContainer);
 //		
 //	}
+	
+	public LinkedList<Shape3D> getFX3DShapes() {
+
+		LinkedList<Shape3D> list = new  LinkedList<>();
+		list.add(new Box(1,2,3));
+		list.add(new Sphere(2));
+		list.add(new Cylinder(1, 5));
+		return list;
+	}
+
+
+	public LinkedList<Text> getFXStringList() {
+		// TODO Auto-generated method stub
+		LinkedList<Text> list = new LinkedList<Text>();
+		list.add (new Text("DN Sormaz"));
+		 return	list;
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
